1c13a7fb4ee303c334ae5e9f8e20492208df02f2
EalhcheGhreaysoun <79121969+EalhcheGhreaysoun@users.noreply.github.com>
2025-03-14 13:28:42 -0600

    hw7 done

--- testFile.cpp
+++ testFile.cpp
@@ -1,59 +1,105 @@
 #include <iostream>
 #include <fstream>
-#include "libs/SplitString.cpp"
+#include <sstream>
+#include <string>
+#include <iomanip>
 
 using namespace std;
 
-void printHikeStats(string file_name){
-    ifstream hikeData(file_name);
-    if (hikeData.fail()){
-        cout << "Could not open file." << endl;
-    }
-    string tokenY;
-    string tokenX;
-    string name[30];
-    string hikeLengthS[30];
-    int hikeLength[30];
-    string elevationGainS[30];
-    int elevationGain[30];
-    int idx = 0;
-    int idy = 0;
+struct Restaurant
+{
+    string name;
+    int food_quality;
+    int cleanliness;
+    int wait_time;
+    double overall;
+};
 
-    //get data into seperate arrays
-    while(getline(hikeData, tokenY)){
-        if(tokenY != ""){
-            stringstream textX(tokenY);
-            while(getline(textX, tokenX, '|')){
-                if(idx == 0){
-                    name[idy] = tokenX;
-                }else if(idx == 1){
-                    hikeLengthS[idy] = tokenX;
-                }else if(idx == 2){
-                    elevationGainS[idy] = tokenX;
+int readRestaurantDetails( string filename, Restaurant restaurant[], const int MAX_RESTAURANTS ){
+    ifstream fileIn(filename);
+    string line;
+    string element;
+    int idx, idy = 0;
+    int numElements = 0;
+
+    if(fileIn.fail()){
+        return -1;
+    }else{
+        while(getline(fileIn, line)){
+            if(line != ""){
+                stringstream lineStream(line);
+                idx = 0;
+                while(getline(lineStream, element, '~')){
+                    numElements++;
                 }
-                idx++;
-                cout << idy << " " << tokenX << endl;
+                element = "";
+                if(numElements == 5){
+                    while(getline(lineStream, element, '~')){
+                        switch (idx)
+                        {
+                            case 0:
+                                restaurant[idy].name = element;
+                                break;
+                            case 1:
+                                restaurant[idy].food_quality = stoi(element);
+                                break;
+                            case 2:
+                                restaurant[idy].cleanliness = stoi(element);
+                                break;
+                            case 3:
+                                restaurant[idy].wait_time = stoi(element);
+                                break;
+                            case 4:
+                                restaurant[idy].overall = stod(element);
+                                break;
+                            default:
+                                break;
+                        }
+                        idx++;
+                    }
+                }
             }
+            idy++;
         }
-        idx = 0;
-        idy++;   
+        return idy;
     }
-
-    for(int i = 0; i < 30; i++){
-        if(hikeLengthS[i] != ""){
-            hikeLength[i] = stoi(hikeLengthS[i]);
-            elevationGain[i] = stoi(elevationGainS[i]);
-        }
-    }
-
-    for(int i = 0; i < 30; i++){
-        cout << elevationGainS[i] << endl;
-        
-    }
-    hikeData.close();
 }
 
-int main(){
-    printHikeStats("testFile.txt");
-    return 0;
+int getBest( Restaurant restaurants[], int arr_size, string metric ){
+    int bestRest = 0;
+    for(int i = 0; i < arr_size; i++){
+        if((metric == "Food Quality")){
+            if((restaurants[i].food_quality > restaurants[bestRest].food_quality)){bestRest = i;}
+        }else if(metric == "Cleanliness"){
+            if((restaurants[i].cleanliness > restaurants[bestRest].cleanliness)){bestRest = i;}
+        }else if(metric == "Wait Time"){
+            if(restaurants[i].wait_time > restaurants[bestRest].wait_time){bestRest = i;}
+        }else if(metric == "Overall"){
+            if(restaurants[i].overall > restaurants[bestRest].overall){bestRest = i;}
+        }else{
+            bestRest = -1;
+        }
+    }
+    return bestRest;
+}
+
+double getOverallRating(Restaurant restaurant){
+    if((restaurant.food_quality>=0 && restaurant.food_quality <=10)&&(restaurant.cleanliness>=0 && restaurant.cleanliness <=10)&&(restaurant.wait_time>=0 && restaurant.wait_time <=5)){
+        return (0.5*restaurant.food_quality) + (0.3*restaurant.cleanliness) + (0.2*restaurant.wait_time);
+    }
+    else{
+        return -1;
+    }
+}
+
+int main()
+{
+    // PART A
+    Restaurant restaurants[30];
+
+    cout << " ." << readRestaurantDetails("testFile.txt", restaurants, 30) << endl;
+    cout << "." << getBest(restaurants, readRestaurantDetails("testFile.txt", restaurants, 30), "Food Qualxty");
+
+    // Checking if the file was opened correctly
+    
 }
